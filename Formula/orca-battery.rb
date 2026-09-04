4f9d9fe6103b94c7f1ae99c5c3a4626d63d179fd
Kridsadar <kridsadar357@gmail.com>
2026-09-04 20:07:08 +0700

    Fix Homebrew resource installation

--- Formula/orca-battery.rb
+++ Formula/orca-battery.rb
@@ -3,6 +3,7 @@ class OrcaBattery < Formula
   homepage "https://github.com/kridsadar357/OrcaBattGuard"
   url "https://github.com/kridsadar357/OrcaBattGuard/archive/refs/tags/v0.7.0.tar.gz"
   sha256 "becf4437cfb2569133361f309010623cc434a96f8bd3394f890a8dac2553b0a7"
+  revision 1
   head "https://github.com/kridsadar357/OrcaBattGuard.git", branch: "main"
 
   depends_on xcode: ["16.0", :build]
@@ -10,12 +11,15 @@ class OrcaBattery < Formula
 
   def install
     system "swift", "build", "-c", "release", "--disable-sandbox", "--product", "orca-battery"
-    bin.install ".build/release/orca-battery" => "orca"
+    libexec.install ".build/release/orca-battery" => "orca"
+    libexec.install ".build/release/OrcaBatteryGuardian_OrcaBatteryGuardian.bundle"
+    bin.write_exec_script libexec/"orca"
     bin.install_symlink "orca" => "orca-battery"
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/orca version")
     assert_match "Orca Battery Guardian CLI", shell_output("#{bin}/orca help")
+    assert_match "Battery:", shell_output("#{bin}/orca status")
   end
 end
